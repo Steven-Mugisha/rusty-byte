c95238b33fd61c7eff91ec4a2a24190b1d03f102
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2023-08-16 22:18:49 -0400

    updated #155

--- 150-LeetCode-Questions/Stack_Queues/#155.py
+++ 150-LeetCode-Questions/Stack_Queues/#155.py
@@ -13,7 +13,7 @@ class MinStack:
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        val = min(val, self.minStack[-1]if self.minStack else val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
         self.minStack.append(val)
 
     def pop(self) -> None:

9f14f86f96fb9460447dff3415ede15b2ebc4484
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2023-10-20 08:02:35 -0400

    Time: 55 ms (91.46%), Space: 16.3 MB (79.6%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.py
+++ 1580-shuffle-the-array/1580-shuffle-the-array.py
@@ -12,6 +12,21 @@ class Solution:
             i += 1
         return res
 
+        # neat code:
+
+        # for i in range(n):
+        #     nums[i] = nums[i] << 10
+        #     nums[i] = nums[i] | nums[i+n] # store x and y
+
+        # j = 2 * n - 1
+        # for i in range(n -1, -1, -1):
+        #     y = nums[i] & (2**10 - 1)
+        #     x = nums[i] >> 10
+        #     nums[j] = y
+        #     nums[j-i] = x
+        #     j -= 2
+        # return nums
+
             
 
 

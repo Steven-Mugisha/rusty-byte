f9d30ba0178a1601f6bf104173e62751bb136463
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2023-10-28 07:21:30 -0400

    Time: 213 ms (62.56%), Space: 19.9 MB (63.54%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.py
+++ 0817-design-hashmap/0817-design-hashmap.py
@@ -1,36 +1,54 @@
 class ListNode:
-    def __init__(self, key=-1, val=-1, next=None):
+    def __init__(self, key=-1, value=-1):
         self.key = key
-        self.val = val
-        self.next = next
+        self.val = value
+        self.next = None
 
 class MyHashMap:
+
     def __init__(self):
         self.map = [ListNode() for i in range(1000)]
-        
-    def hashcode(self, key):
-        return key % len(self.map)
+
+    def hashfunc(self, key):
+        hash = key % len(self.map)
+        return hash 
 
     def put(self, key: int, value: int) -> None:
-        cur = self.map[self.hashcode(key)]
+        index = self.hashfunc(key)
+        cur = self.map[index]
+
         while cur.next:
             if cur.next.key == key:
                 cur.next.val = value
-                return
+                return 
             cur = cur.next
         cur.next = ListNode(key, value)
-         
+        
     def get(self, key: int) -> int:
-        cur = self.map[self.hashcode(key)].next
+        index = self.hashfunc(key)
+        cur = self.map[index].next
+
         while cur and cur.key != key:
             cur = cur.next
         if cur:
             return cur.val
         return -1
 
+
     def remove(self, key: int) -> None:
-        cur = self.map[self.hashcode(key)]
+        index = self.hashfunc(key)
+        cur = self.map[index]
+
         while cur.next and cur.next.key != key:
             cur = cur.next
         if cur and cur.next:
-            cur.next = cur.next.next
+            cur.next = cur.next.next 
+        
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)

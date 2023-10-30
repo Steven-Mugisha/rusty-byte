e8b10a8be587255ddae05540530db90462b51135
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2023-10-30 08:32:17 -0400

    Time: 86 ms (43.62%), Space: 16.3 MB (42.93%) - LeetHub

--- 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.py
+++ 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.py
@@ -1,6 +1,7 @@
 class Solution:
     def numberOfPoints(self, nums: List[List[int]]) -> int:
         Mapset = set()
+        
         for i in nums:
             for j in range(i[0], i[1] + 1):
                 Mapset.add(j)

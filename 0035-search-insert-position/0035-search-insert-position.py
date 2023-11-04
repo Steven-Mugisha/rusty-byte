5c13167e4e291b333ebefe9ac954d89f2c30ca87
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2023-11-04 08:07:01 -0400

    Time: 59 ms (24.31%), Space: 17.2 MB (24.75%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,10 +1,6 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
 
-        # if target in nums : return index
-        # else return index place
-        # [1,3,5,6], if nums[R] < target and nums[L] > target: return index
-
         L, R = 0, len(nums) - 1
 
         while L <= R:
@@ -20,6 +16,6 @@ class Solution:
             else:
                 L = mid + 1
 
-        return R + 1
+        return L
 
         

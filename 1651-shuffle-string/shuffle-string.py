4653442b4fb5d73d42c9f634eea30bdbd1277c8d
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2023-12-07 23:27:18 -0500

    Time: 71 ms (6.39%) | Memory: 16.4 MB (8.24%) - LeetSync

--- 1651-shuffle-string/shuffle-string.py
+++ 1651-shuffle-string/shuffle-string.py
@@ -5,8 +5,10 @@ class Solution:
 
         for i in range(len(s)):
             Map[indices[i]] = s[i]
+
         j = 0
         final_string = ''
+        
         while j < len(indices):
           final_string += ''.join(Map[j])
           j += 1

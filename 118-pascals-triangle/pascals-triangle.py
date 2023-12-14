c42d074120641cadd3f78c556c1c2771665dff89
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2023-12-14 07:23:16 -0500

    Time: 41 ms (50.68%) | Memory: 16.1 MB (85.60%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.py
@@ -0,0 +1,24 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+
+        res = [[1]]
+
+        for i in range(numRows -1):
+            curr_row = [0] + res[-1] + [0]
+            
+            new_row = []
+            for j in range(len(curr_row) -1):
+                sum_vals = curr_row[j] + curr_row[j+1]
+                new_row.append(sum_vals)
+
+            res.append(new_row)
+        
+        return res
+
+
+
+        
+
+
+
+        

de6864e8304826e54f48561874c7ac70cb18f719
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-02-17 09:10:49 -0500

    Time: 37 ms (60.39%) | Memory: 16.6 MB (77.75%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -3,17 +3,19 @@ class Solution:
 
         res = [[1]]
 
-        for i in range(numRows -1):
+        for i in range(numRows - 1):
             curr_row = [0] + res[-1] + [0]
-            
-            new_row = []
-            for j in range(len(curr_row) -1):
-                sum_vals = curr_row[j] + curr_row[j+1]
-                new_row.append(sum_vals)
 
+            new_row = []
+            for j in range(len(curr_row) - 1):
+                new_row.append(curr_row[j] + curr_row[j+1])
             res.append(new_row)
-        
+
         return res
+
+
+
+
 
 
 

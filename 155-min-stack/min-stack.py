e0f5000084e9de86781946632a07cab1fdb24860
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-02-29 20:10:24 -0500

    Time: 57 ms (50.38%) | Memory: 20.4 MB (77.44%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -1,21 +1,20 @@
 class MinStack:
 
     def __init__(self):
-        self.Stack = []
+        self.stack = []
         self.minStack = []
 
     def push(self, val: int) -> None:
-        self.Stack.append(val)
+        self.stack.append(val)
         val = min(val, self.minStack[-1] if self.minStack else val)
         self.minStack.append(val)
-    
+
     def pop(self) -> None:
-        self.Stack.pop()
-        # pop it from minStack as well as it might indeed be the minimum element in the Stack. 
-        self.minStack.pop() 
+        self.stack.pop()
+        self.minStack.pop()
 
     def top(self) -> int:
-        return self.Stack[-1]
+        return self.stack[-1]
         
     def getMin(self) -> int:
         return self.minStack[-1]

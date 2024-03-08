008b836498ab261d1e5f980eca2095edbc6bfd7a
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-03-08 07:22:11 -0500

    Time: 46 ms (72.83%) | Memory: 17 MB (27.59%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,55 +1,17 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # find the index j such that nums1[i] == nums2[j]
-        # determine the next greater element of nums2[j] in nums2: return it or otherwise return -1.
+        nums1Idx = {val: idx for idx, val in enumerate(nums1)}
 
-        # create a hash map to store the values of nums2 so that we can keep track of the indexs
-        # do a for loop until the length of nums1 
-        # check if there exist and element from the nums2[j] +1 if then return else return -1
-        # append to res arr and then return.
-
-        # create the map for to keep nums2 values:
-
-        Map = {}
-
-        for idx, val in enumerate(nums1):
-            Map[val] = idx
-
-        res = [-1]*len(nums1)
         stack = []
+        res = [-1]*len(nums1)
 
-        # Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
         for i in range(len(nums2)):
             curr = nums2[i]
-            while stack and stack[-1] < curr:
+            while stack and curr > stack[-1]:
                 val = stack.pop()
-                index = Map[val]
-                res[index] = curr
-            
-            if curr in Map:
+                idx = nums1Idx[val]
+                res[idx] = curr
+            if curr in nums1Idx:
                 stack.append(curr)
 
         return res
-        # for i in range(len(nums1)):
-        #     val = nums1[i]
-        #     if val in Map:
-        #         idx = Map[val]
-        #         for j in range(idx + 1, len(nums2)):
-        #             if nums2[j] > val:
-        #                 res[i] = nums2[j]
-        #                 break
-                    
-        #             else:
-        #                 continue
-        
-        # return res
-
-        
-
-
-
-
-        
-
-       
-

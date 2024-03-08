3d80e6369385b45e7002807ac1cc9e14bc13ee3a
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-03-07 20:34:27 -0500

    Time: 41 ms (91.32%) | Memory: 17.1 MB (20.08%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -0,0 +1,41 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # find the index j such that nums1[i] == nums2[j]
+        # determine the next greater element of nums2[j] in nums2: return it or otherwise return -1.
+
+        # create a hash map to store the values of nums2 so that we can keep track of the indexs
+        # do a for loop until the length of nums1 
+        # check if there exist and element from the nums2[j] +1 if then return else return -1
+        # append to res arr and then return.
+
+        # create the map for to keep nums2 values:
+        Map = {}
+
+        for idx, val in enumerate(nums2):
+            Map[val] = idx
+
+        res = [-1]*len(nums1)
+
+        for i in range(len(nums1)):
+            val = nums1[i]
+            if val in Map:
+                idx = Map[val]
+                for j in range(idx + 1, len(nums2)):
+                    if nums2[j] > val:
+                        res[i] = nums2[j]
+                        break
+                    
+                    else:
+                        continue
+        
+        return res
+
+        
+
+
+
+
+        
+
+       
+

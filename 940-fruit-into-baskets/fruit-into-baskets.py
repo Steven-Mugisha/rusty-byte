3773bebc041c14f2231a40a836b97acdf5627439
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-03-22 23:23:37 -0400

    Time: 671 ms (69.30%) | Memory: 24.1 MB (53.14%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,49 +1,22 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        start = 0
-        max_sum = 0
-        # trees = 2
-        # counter = defaultdict(trees)
-        counter = {}
+        l,ans, total = 0,0,0
+        counter = defaultdict(int)
 
-        for idx, fruit in enumerate(fruits):
+        for r in range(len(fruits)):
+            counter[fruits[r]] += 1
+            total += 1
 
-            if fruit in counter:
-                counter[fruit] += 1
-            
-            else:
-                counter[fruit] = 1
+            while len(counter) > 2:
+                f = fruits[l]
+                counter[f] -= 1
+                total -= 1
 
-            if len(counter.keys()) <= 2:
-                curr_sum = 0
-                for key, val in counter.items():
-                    curr_sum += val 
-                max_sum = max(max_sum, curr_sum)            
+                if counter[f] == 0:
+                    del counter[f]
+                    
+                l += 1
 
-            while len(counter.keys()) > 2:
-                counter[fruits[start]] -= 1
-
-                if counter[fruits[start]] == 0: 
-                    del counter[fruits[start]]
-
-                start += 1
-
-        return max_sum
-            
-           
-
-
-
-            
-
-
-
-
-
-
-
-
-
-
-
-
+            ans = max(ans, total)
+        return ans
+        

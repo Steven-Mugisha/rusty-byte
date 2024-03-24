a3a6e3a1265b9f31ac7b8d21eb4a3ff639627655
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-03-24 08:35:29 -0400

    Time: 39 ms (41.93%) | Memory: 16.5 MB (89.88%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.py
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.py
@@ -4,7 +4,6 @@ class Solution:
 
         for c in range(len(s)-2):
             if s[c] != s[c+1] and s[c] != s[c+2] and s[c+1] != s[c+2]:
-                print(s[c], s[c+1], s[c+2])
                 ans += 1
     
         return ans

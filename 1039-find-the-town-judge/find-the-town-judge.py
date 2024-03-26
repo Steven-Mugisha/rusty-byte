bb79d33c63a596bd76d35e2e13672b4108653079
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-03-26 12:36:15 -0400

    Time: 606 ms (50.05%) | Memory: 21.6 MB (44.86%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -2,7 +2,7 @@ class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         if n == 1:
             return 1
-
+        
         indegree = defaultdict(int)
         outdegree = defaultdict(int)
 
@@ -10,10 +10,8 @@ class Solution:
             indegree[trusted] += 1
             outdegree[trusts] += 1
         
-
-        for v, k in indegree.items():
+        for v,k in indegree.items():
             if k == n-1 and outdegree[v] == 0:
                 return v
-        
-        return - 1
-        
+            
+        return -1 

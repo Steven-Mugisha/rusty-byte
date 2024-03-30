372381f429d5db1b97bda0791d981a53d8c17aa8
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-03-30 11:29:35 -0400

    Time: 306 ms (18.13%) | Memory: 24.2 MB (31.87%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -4,27 +4,26 @@ class Solution:
             return 0
 
         rows, cols = len(grid), len(grid[0])
-        seen = set()
         island = 0
+        seen = set()
 
-        def bfs(r, c):
-            queue = collections.deque()
-            queue.append((r,c))
+        def bfs(r,c):
+            queue = [(r,c)]
             seen.add((r,c))
-            
+
             while queue:
-                row, col = queue.popleft()
-                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                row, col = queue.pop(0)
+                directions = [[1, 0], [-1, 0],[0, 1], [0, -1]]
 
                 for dr, dc in directions:
                     r, c = dr + row, dc + col
                     if (
-                        r in range(rows) and c in range(cols) and 
-                        (r, c) not in seen and grid[r][c] == '1'):
+                        r in range(rows) and c in range(cols) and
+                        grid[r][c] == '1' and (r, c) not in seen
+                    ):
                         seen.add((r,c))
-                        queue.append((r, c))
-
-       
+                        queue.append((r,c))
+        
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == '1' and (r, c) not in seen:

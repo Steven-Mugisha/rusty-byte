82baf8cdb26f36fc3a6953a016758571609bbd9d
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-04-01 08:39:23 -0400

    Time: 310 ms (15.97%) | Memory: 24.4 MB (23.41%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,33 +1,33 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if len(grid) == 0:
-            return 0
 
         rows, cols = len(grid), len(grid[0])
-        island = 0
         seen = set()
+        island = 0
 
         def bfs(r,c):
-            queue = [(r,c)]
+            queue = collections.deque()
+            queue.append((r,c))
             seen.add((r,c))
 
             while queue:
-                row, col = queue.pop(0)
-                directions = [[1, 0], [-1, 0],[0, 1], [0, -1]]
+                row, col = queue.popleft()
+                directions = [[1,0], [-1,0], [0,1], [0,-1]]
 
                 for dr, dc in directions:
                     r, c = dr + row, dc + col
                     if (
-                        r in range(rows) and c in range(cols) and
-                        grid[r][c] == '1' and (r, c) not in seen
+                        r in range(rows) and c in range(cols) and 
+                        (r,c) not in seen and grid[r][c] == "1"
                     ):
                         seen.add((r,c))
                         queue.append((r,c))
-        
+
+
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == '1' and (r, c) not in seen:
+                if grid[r][c] == "1" and (r,c) not in seen:
                     bfs(r, c)
                     island += 1
-        
+
         return island

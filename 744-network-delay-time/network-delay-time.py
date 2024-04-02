36112937147ebe324b1216d349cce6a4bbf3135f
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-04-02 08:15:20 -0400

    Time: 427 ms (11.20%) | Memory: 19.6 MB (5.83%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,32 +1,33 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        from collections import defaultdict
         from queue import PriorityQueue
 
-        graph = defaultdict(list)
+        graph = collections.defaultdict(list)
+
         for u,v,w in times:
             graph[u].append((v,w))
 
         pq = PriorityQueue()
         pq.put((0,k))
-        seen = set()
-        delay_times = 0
+
+        delay_times, seen = 0, set()
 
         while not pq.empty():
             time, node = pq.get()
 
             if node in seen:
                 continue
-
-            seen.add(node)
-            delay_times = max(delay_times, time)
-            neighbours = graph[node]
             
+            delay_times = max(delay_times, time)
+            seen.add(node)
+            neighbours = graph[node]
             for neighbour, t in neighbours:
                 if neighbour not in seen:
-                    pq.put((t + time, neighbour))
-        
+                    pq.put((t+time, neighbour))
+  
+
         if len(seen) == n:
             return delay_times
         
         return -1
+

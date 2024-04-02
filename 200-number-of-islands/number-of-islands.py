2f4c90bc0369240621274472aec70a84970084b7
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-04-02 19:59:19 -0400

    Time: 304 ms (19.16%) | Memory: 24 MB (39.60%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,31 +1,29 @@
 class Solution:
-    from collections import deque
     def numIslands(self, grid: List[List[str]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        island, seen = 0, set()
+        seen, island = set(), 0
 
-        def bfs(r,c):
+        def bfs(r, c):
             q = collections.deque()
             q.append((r,c))
 
             while q:
-                row,col = q.popleft()
+                row, col = q.popleft()
                 directions = [(1,0), (-1,0), (0,1), (0,-1)]
-
-                for dr,dc in directions:
-                    r,c = dr + row, dc + col
+                for dr, dc in directions:
+                    r, c = dr + row, dc + col
                     if (
-                        r in range(rows) and c in range(cols) and
-                        (r,c) not in seen and grid[r][c] == "1"
+                        r in range(rows) and c in range(cols) and 
+                        (r,c) not in seen and grid[r][c] == '1'
                     ):
                         seen.add((r,c))
                         q.append((r,c))
-            
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r,c) not in seen:
+                if grid[r][c] == '1' and (r,c) not in seen:
                     bfs(r,c)
                     island += 1
         
         return island
+

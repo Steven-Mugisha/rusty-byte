217f3986053efe3fbbc35f8d4a61735b6847ac70
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-04-03 18:25:43 -0400

    Time: 40 ms (59.23%) | Memory: 17 MB (9.42%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -12,20 +12,18 @@ class Solution:
         nodes_completed = {}
 
         def dfs(node):
-            if not node:
+            if node is None:
                 return None
-                
-            cloned_node = Node(node.val)
-            nodes_completed[node] = cloned_node
+            copied_node = Node(node.val)
+            nodes_completed[node] = copied_node
 
             for nei in node.neighbors:
                 curr_node = nodes_completed.get(nei)
                 if not curr_node:
-                    cloned_node.neighbors.append(dfs(nei))
+                    copied_node.neighbors.append(dfs(nei))
                 else:
-                    cloned_node.neighbors.append(curr_node)
+                    copied_node.neighbors.append(curr_node)
             
-            return cloned_node
-        
+            return copied_node
+
         return dfs(node)
-        

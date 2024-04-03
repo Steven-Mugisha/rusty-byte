7c9068f75573353d8cada4e01f3ef498f48d3e3c
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-04-02 21:52:00 -0400

    Time: 38 ms (75.94%) | Memory: 16.9 MB (34.57%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -11,25 +11,27 @@ class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
 
         nodes_completed = {}
-        return self.cloneGraph_helper(node, nodes_completed)
+
+        def cloneGraph_helper(node):
+            if node is None:
+                return None
+
+            cloned_node = Node(node.val)
+            nodes_completed[node] = cloned_node
+
+            for p in node.neighbors:
+                x = nodes_completed.get(p)
+                if not x:
+                    cloned_node.neighbors += [cloneGraph_helper(p)]
+                
+                else:
+                    cloned_node.neighbors += [x]
+
+            return cloned_node
+
+        return cloneGraph_helper(node)
 
 
-    def cloneGraph_helper(self, node, nodes_completed):
-        if node is None:
-            return None
-
-        cloned_node = Node(node.val)
-        nodes_completed[node] = cloned_node
-
-        for p in node.neighbors:
-            x = nodes_completed.get(p)
-            if not x:
-                cloned_node.neighbors += [self.cloneGraph_helper(p, nodes_completed)]
-            
-            else:
-                cloned_node.neighbors += [x]
-
-        return cloned_node
 
 
 

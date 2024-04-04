8b7be08fed15e87b4f278e8c6185cad3db187b1e
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-04-04 08:00:18 -0400

    Time: 114 ms (37.48%) | Memory: 16.7 MB (49.57%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,11 +1,6 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-
-        # "AABABBA",     
-        # k = 1, map.keys() == 2, key minimum value: B: 1, >= k = 1, curr_max
-        # {A:2, B:1} == 5
-
-        counter = collections.defaultdict(int)
+        counter = defaultdict(int)
         l = 0
         curr_max = 0
         result = 0
@@ -13,24 +8,12 @@ class Solution:
         for r in range(len(s)):
             curr_max += 1
             counter[s[r]] += 1
-            
-            while curr_max - max(counter.values()) > k:
+
+            while (curr_max - max(counter.values())) > k:
                 counter[s[l]] -= 1
                 curr_max -= 1
                 l += 1
-
+            
             result = max(result, curr_max)
-            
-        return result
-
-
         
-            
-
-
-
-
-
-
-
-
+        return result

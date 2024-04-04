d4976773d0724156f673740a95608798ed8d767c
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-04-04 08:36:41 -0400

    Time: 1189 ms (32.49%) | Memory: 41.5 MB (50.98%) - LeetSync

--- 833-bus-routes/bus-routes.py
+++ 833-bus-routes/bus-routes.py
@@ -1,24 +1,24 @@
-from collections import deque, defaultdict
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
-        graph = defaultdict(list)
+        connections = defaultdict(list)
         for i, stations in enumerate(routes):
             for station in stations:
-                graph[station].append(i)
+                connections[station].append(i)
         
+        seen = set()
         q = deque()
         q.append([source, 0])
-        seen = set()
 
         while q:
             station, bus_taken = q.popleft()
             if station == target:
                 return bus_taken
-
-            if station in graph:
-                for bus in graph[station]:
+            
+            if station in connections:
+                for bus in connections[station]:
                     if bus not in seen:
+                        for st in routes[bus]:
+                            q.append([st, bus_taken + 1])
                         seen.add(bus)
-                        for s in routes[bus]:
-                            q.append([s, bus_taken + 1])
-        return -1 
+        return -1
+

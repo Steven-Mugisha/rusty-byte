926f3c49136a3dc7a701a1395ff0fa7127c739ad
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-04-07 08:23:04 -0400

    Time: 94 ms (52.90%) | Memory: 16.6 MB (89.66%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,19 +1,17 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         counter = defaultdict(int)
-        l = 0
-        curr_max = 0
-        result = 0
+        l, res, currLength = 0, 0, 0
 
         for r in range(len(s)):
-            curr_max += 1
+            currLength += 1
             counter[s[r]] += 1
-
-            while (curr_max - max(counter.values())) > k:
+            maxCount = max(counter.values())
+            while (currLength - maxCount) > k:
                 counter[s[l]] -= 1
-                curr_max -= 1
+                currLength -= 1
                 l += 1
             
-            result = max(result, curr_max)
+            res = max(res, currLength)
         
-        return result
+        return res

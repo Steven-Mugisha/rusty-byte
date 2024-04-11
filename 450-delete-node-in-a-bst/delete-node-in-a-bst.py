157510bb3013c1a3787ee705e1d42ba3b62d70f8
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-04-11 06:52:29 -0400

    Time: 44 ms (97.56%) | Memory: 19.7 MB (99.65%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.py
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -4,40 +4,35 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        return self.__delete_node(root, key)
-    
-    def __find_min(self, current_node):
-        while current_node.left:
-            current_node = current_node.left
-        
-        return current_node.val
-
-    def __delete_node(self, current_node, key):
-        if current_node is None:
+        if root == None:
             return None
-
-        if key < current_node.val:
-            current_node.left = self.__delete_node(current_node.left, key)
-
-        elif key > current_node.val:
-            current_node.right = self.__delete_node(current_node.right, key)
-
+        
+        elif root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        
         else:
-            if current_node.left is None and current_node.right is None:
+            if not root.left and not root.right:
                 return None
-
-            elif current_node.left is None:
-                current_node = current_node.right
-
-            elif current_node.right is None:
-                current_node = current_node.left
-
+            
+            elif not root.left:
+                root = root.right
+            
+            elif not root.right:
+                root = root.left
+            
             else:
-                sub_tree_min = self.__find_min(current_node.right)
-                current_node.val = sub_tree_min
-                current_node.right = self.__delete_node(current_node.right, sub_tree_min)
+                root.val = self.find_min_val(root.right)
+                root.right = self.deleteNode(root.right, root.val)
+        return root
+    
+    def find_min_val(self, root):
+        while root.left:
+            root = root.left   
+        return root.val
+        
 
-        return current_node

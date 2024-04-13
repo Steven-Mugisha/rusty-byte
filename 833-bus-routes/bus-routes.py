3fdd92604d997d02e826a41febd56eafb92c8d54
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-04-12 21:45:25 -0400

    Time: 1101 ms (41.49%) | Memory: 41.4 MB (49.37%) - LeetSync

--- 833-bus-routes/bus-routes.py
+++ 833-bus-routes/bus-routes.py
@@ -1,25 +1,24 @@
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        graph = defaultdict(list)
 
-        connections = defaultdict(list)
         for bus, stations in enumerate(routes):
             for station in stations:
-                connections[station].append(bus)
+                graph[station].append(bus)
         
+        seen = set()
+
         q = deque()
         q.append([source, 0])
-        seen = set()
 
         while q:
             station, buses_taken = q.popleft()
             if station == target:
                 return buses_taken
             
-            if station in connections:
-                for bus in connections[station]:
-                    if bus not in seen:
-                        seen.add(bus)
-                        for st in routes[bus]:
-                            q.append([st, buses_taken + 1])
+            for bus in graph[station]:
+                if bus not in seen:
+                    seen.add(bus)
+                    for st in routes[bus]:
+                        q.append([st, buses_taken + 1])
         return -1
-                            

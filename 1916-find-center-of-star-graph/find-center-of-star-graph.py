28e6945c5223484ebfae7abc853726d6187903ec
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-04-17 08:48:31 -0400

    Time: 667 ms (54.25%) | Memory: 52.4 MB (47.05%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.py
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.py
@@ -3,5 +3,7 @@ class Solution:
 
         if edges[0][0] == edges[1][0] or edges[0][0] == edges[1][1]:
             return edges[0][0]
+        
         else:
             return edges[0][1]
+            

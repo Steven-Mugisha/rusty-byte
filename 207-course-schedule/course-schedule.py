da93d48704365a215c18c4df082ba0a21c1f8f6d
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-04-19 08:43:51 -0400

    Time: 91 ms (51.46%) | Memory: 18.3 MB (81.03%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        courses = collections.defaultdict(list)
+
+        for course, preq in prerequisites:
+            courses[course].append(preq)
+
+        stack = [0]
+        seen = set()
+        visiting = set()
+
+        def has_cycle(course):
+            if course in visiting:
+                return True
+            if course in seen:
+                return False
+
+            visiting.add(course)
+            for preq in courses[course]:
+                if has_cycle(preq):
+                    return True
+            visiting.remove(course)
+            seen.add(course)
+            return False
+
+        for course in range(numCourses):
+            if has_cycle(course):
+                return False
+
+        return True
+            
+
+
+

7b6639dcabc6023a5fca6c4a07a6bd818ce9c34f
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-04-23 20:53:30 -0400

    Time: 246 ms (17.05%) | Memory: 18.3 MB (22.18%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,33 +1,34 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        rows, cols = len(heights), len(heights[0])
-
+        ROWS, COLS = len(heights), len(heights[0])
+        pco, ato = set(), set()
         res = []
-        pc, atl = set(), set()
 
         def dfs(r,c, visited, prevHeight):
-            if (r not in range(rows) or 
-                c not in range(cols) or heights[r][c] < prevHeight or 
+            if (r not in range(ROWS) or c not in range(COLS) or
+                heights[r][c] < prevHeight or
                 (r,c) in visited):
-                return 
+                return
             
             visited.add((r,c))
             directions = [(1,0), (-1,0), (0,1), (0,-1)]
             for dr, dc in directions:
                 row, col = dr + r, dc + c
                 dfs(row, col, visited, heights[r][c])
-    
-        for c in range(cols):
-            dfs(0, c, pc, heights[0][c])
-            dfs(rows - 1, c, atl, heights[rows -1][c])
+
+
+        for c in range(COLS):
+            dfs(0, c, pco, heights[0][c])
+            dfs(ROWS-1, c, ato, heights[ROWS-1][c])
         
-        for r in range(rows):
-            dfs(r, 0, pc, heights[r][0])
-            dfs(r, cols -1, atl, heights[r][cols -1])
+        for r in range(ROWS):
+            dfs(r, 0, pco, heights[r][0])
+            dfs(r, COLS-1, ato, heights[r][COLS-1])
         
-        for r in range(rows):
-            for c in range(cols):
-                if (r, c) in pc and (r,c) in atl:
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r,c) in pco and (r,c) in ato:
                     res.append([r,c])
         
         return res
+                    

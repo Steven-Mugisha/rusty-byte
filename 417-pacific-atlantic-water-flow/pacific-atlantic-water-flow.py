df0c52ff217d83f3a31be45cc93ddac05ea3af7e
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-04-24 22:31:39 -0400

    Time: 224 ms (32.40%) | Memory: 18.4 MB (22.01%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,12 +1,13 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         ROWS, COLS = len(heights), len(heights[0])
-        pc, at = set(), set()
-        res = []
+        pc , at = set(), set()
+        result = []
 
-        def dfs(r,c, visited, prevHeight):
-            if (r not in range(ROWS) or c not in range(COLS) or heights[r][c] < prevHeight or (r,c) in visited):
+        def dfs(r,c, visited, prevHeights):
+            if (r not in range(ROWS) or c not in range(COLS) or heights[r][c] < prevHeights or (r,c) in visited):
                 return 
+            
             visited.add((r,c))
             directions = [(1,0), (-1,0), (0,1), (0,-1)]
             for dr, dc in directions:
@@ -24,7 +25,8 @@ class Solution:
         for r in range(ROWS):
             for c in range(COLS):
                 if (r,c) in pc and (r,c) in at:
-                    res.append([r,c])
-        
-        return res
+                    result.append([r,c])
+        return result
 
+
+        

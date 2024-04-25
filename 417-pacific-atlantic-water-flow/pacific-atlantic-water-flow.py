27bf9857a23c1d19b4f96708b564779b76eaf10f
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-04-25 08:14:12 -0400

    Time: 245 ms (17.18%) | Memory: 18.3 MB (33.53%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,32 +1,30 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        ROWS, COLS = len(heights), len(heights[0])
-        pc , at = set(), set()
+        rows, cols = len(heights), len(heights[0])
+        pco, ato = set(), set()
         result = []
 
         def dfs(r,c, visited, prevHeights):
-            if (r not in range(ROWS) or c not in range(COLS) or heights[r][c] < prevHeights or (r,c) in visited):
+            if (r not in range(rows) or c not in range(cols) or heights[r][c] < prevHeights or (r,c) in visited):
                 return 
-            
             visited.add((r,c))
-            directions = [(1,0), (-1,0), (0,1), (0,-1)]
+            directions = [(1,0),(-1,0),(0,1),(0,-1)]
             for dr, dc in directions:
-                row, col = dr + r, dc + c
+                row, col = dr + r,  dc + c
                 dfs(row, col, visited, heights[r][c])
 
-        for c in range(COLS):
-            dfs(0, c, pc, heights[0][c])
-            dfs(ROWS-1, c, at, heights[ROWS-1][c])
+        for c in range(cols):
+            dfs(0, c, pco, heights[0][c])
+            dfs(rows-1, c, ato, heights[rows-1][c])
         
-        for r in range(ROWS):
-            dfs(r, 0, pc, heights[r][0])
-            dfs(r, COLS-1, at, heights[r][COLS-1])
+        for r in range(rows):
+            dfs(r, 0, pco, heights[r][0])
+            dfs(r, cols-1, ato, heights[r][cols-1])
         
-        for r in range(ROWS):
-            for c in range(COLS):
-                if (r,c) in pc and (r,c) in at:
+
+        for r in range(rows):
+            for c in range(cols):
+                if (r,c) in pco and (r,c) in ato:
                     result.append([r,c])
-        return result
-
-
         
+        return result

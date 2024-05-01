5c3da17b44b15b37b5f2de6d6a516d620cdfc3ec
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-04-30 21:55:41 -0400

    Time: 38 ms (72.61%) | Memory: 16.6 MB (11.22%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
@@ -12,16 +12,17 @@ class Solution:
             return head
         
         slow = head
-        # fast = head.next
+        fast = head.next
 
         while slow.next is not None:
-            if slow.val == slow.next.val:
+            if slow.val == fast.val:
                 slow.next = slow.next.next
                 # slow = fast.next
-                # fast = fast.next
+                fast = fast.next
             
             else:
                 slow = slow.next
-                # fast = fast.next
+                fast = fast.next
         
         return head
+

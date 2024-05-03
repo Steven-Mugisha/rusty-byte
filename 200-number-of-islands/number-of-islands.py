18fd46bf86f499e64298735eca8ac6549f218488
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-05-02 23:28:31 -0400

    Time: 303 ms (14.61%) | Memory: 23.9 MB (35.55%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,29 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows, cols = len(grid), len(grid[0])
+        ROWS, COLS = len(grid), len(grid[0])
 
-        seen, island = set(), 0
+        island, visited = 0, set()
 
         def bfs(r,c):
-            q = deque()
+            q = collections.deque()
             q.append((r,c))
 
             while q:
                 row, col = q.popleft()
                 directions = [(1,0), (-1,0), (0,1), (0,-1)]
-
                 for dr, dc in directions:
-                    r,c = dr + row, dc + col
-                    if ( r in range(rows) and c in range(cols) and grid[r][c] == '1' and (r,c) not in seen):
-                        seen.add((r,c))
+                    r, c = dr + row, dc + col
+                    if (r in range(ROWS) and c in range(COLS) and grid[r][c] == '1' and (r,c) not in visited):
+                        visited.add((r,c))
                         q.append((r,c))
 
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == '1' and (r,c) not in seen:
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == '1' and (r,c) not in visited:
                     bfs(r,c)
                     island += 1
         
         return island
-
-        

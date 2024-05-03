6563738f3c9b0959b964883c9cbf1f16b32e584b
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-05-03 08:14:21 -0400

    Time: 237 ms (20.68%) | Memory: 18.2 MB (33.53%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,30 +1,32 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        rows, cols = len(heights), len(heights[0])
-        pco, ato = set(), set()
-        result = []
+        ROWS, COLS = len(heights), len(heights[0])
+
+        pacific, atlantic = set(), set()
+        canFlow = []
 
         def dfs(r,c, visited, prevHeights):
-            if (r not in range(rows) or c not in range(cols) or heights[r][c] < prevHeights or (r,c) in visited):
+            if (r not in range(ROWS) or c not in range(COLS) or heights[r][c] < prevHeights or (r,c) in visited):
                 return 
+            directions = [(1,0), (-1,0), (0,1), (0,-1)]
             visited.add((r,c))
-            directions = [(1,0),(-1,0),(0,1),(0,-1)]
             for dr, dc in directions:
-                row, col = dr + r,  dc + c
+                row, col = dr + r, dc + c
                 dfs(row, col, visited, heights[r][c])
 
-        for c in range(cols):
-            dfs(0, c, pco, heights[0][c])
-            dfs(rows-1, c, ato, heights[rows-1][c])
+    
+        for c in range(COLS):
+            dfs(0, c, pacific, heights[0][c])
+            dfs(ROWS -1, c, atlantic, heights[ROWS -1][c])
         
-        for r in range(rows):
-            dfs(r, 0, pco, heights[r][0])
-            dfs(r, cols-1, ato, heights[r][cols-1])
+        for r in range(ROWS):
+            dfs(r, 0, pacific, heights[r][0])
+            dfs(r, COLS -1, atlantic, heights[r][COLS - 1])
         
-
-        for r in range(rows):
-            for c in range(cols):
-                if (r,c) in pco and (r,c) in ato:
-                    result.append([r,c])
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r,c) in pacific and (r,c) in atlantic:
+                    canFlow.append([r,c])
+    
+        return canFlow
         
-        return result

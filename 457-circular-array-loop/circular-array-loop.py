4504f57c7f234bc9602e61626e4349f683f08284
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-05-05 11:09:46 -0400

    Time: 543 ms (10.55%) | Memory: 16.6 MB (94.01%) - LeetSync

--- 457-circular-array-loop/circular-array-loop.py
+++ 457-circular-array-loop/circular-array-loop.py
@@ -1,40 +1,40 @@
 class Solution:
     def circularArrayLoop(self, nums: List[int]) -> bool:
         size = len(nums)
+
+        def nextStep(pointer, value, size):
+            step = (pointer + value) % size
+            if step < 0:
+                step += size
+            return step
+
+        def isNotCycle(nums, PreDirection, pointer):
+            currentDirection = nums[pointer] >= 0
+            if (currentDirection != PreDirection) or (abs(nums[pointer]) % len(nums) == 0):
+                return True
+            
+            return False
+
         for i in range(size):
             slow = fast = i
             forward = nums[i] > 0
-        
-            while True:
-                slow = self.next_step(slow, nums[slow], size) 
-                if self.is_not_cycle(nums, forward, slow):
-                    break
-            
-                fast = self.next_step(fast, nums[fast], size)
-                if self.is_not_cycle(nums, forward, fast):
+
+            while (True):
+                slow = nextStep(slow, nums[slow], size)
+                if isNotCycle(nums, forward, slow):
                     break
                 
-                fast = self.next_step(fast, nums[fast], size)
-                if self.is_not_cycle(nums, forward, fast):
+                fast = nextStep(fast, nums[fast], size)
+                if isNotCycle(nums, forward, fast):
                     break
-            
+                
+                fast = nextStep(fast, nums[fast], size)
+                if isNotCycle(nums, forward,fast):
+                        break
+                
                 if slow == fast:
                     return True
-                
+        
         return False
 
-    # A function to calculate the next step
-    def next_step(self, pointer, value, size):
-        result = (pointer + value) % size
-        if result < 0:
-            result += size
-        return result
-
-    # A function to detect a cycle doesn't exist
-    def is_not_cycle(self, nums, prev_direction, pointer):
-
-            curr_direction = nums[pointer] >= 0
-            if (prev_direction != curr_direction) or (abs(nums[pointer] % len(nums)) == 0):
-                return True
-            else:
-                return False
+            

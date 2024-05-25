0550cc5372a55c632ffea1351ed90b24779b1fa5
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-05-25 09:28:07 -0400

    Time: 64 ms (56.06%) | Memory: 20.9 MB (47.08%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -6,31 +6,31 @@
 #         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        def max_sum_helper(root):
+        def maxPathSum_rec(root):
             if not root:
                 return 0
             
-            root_leftSum = max_sum_helper(root.left)
-            root_rightSum = max_sum_helper(root.right)
+            leftTreeSum = maxPathSum_rec(root.left)
+            rightTreeSum = maxPathSum_rec(root.right)
 
-            leftTree, rightTree = 0,0
+            left, right = 0, 0
 
-            if root_leftSum > 0:
-                leftTree = root_leftSum
+            if leftTreeSum > 0: left = leftTreeSum
+            if rightTreeSum > 0: right = rightTreeSum
+
+            currentPathSum = root.val + left + right
+
+            maxSum = maxPathSum_rec.maxSum
+            maxPathSum_rec.maxSum = max(maxSum, currentPathSum)
+
+            return root.val + max(left, right)
+
+        maxPathSum_rec.maxSum = float("-inf")
             
-            if root_rightSum > 0:
-                rightTree = root_rightSum
-            
-            valSum_newPath = root.val + leftTree + rightTree
-            
-            max_sum = max_sum_helper.max_sum
+        maxPathSum_rec(root)
 
-            max_sum_helper.max_sum = max(max_sum, valSum_newPath)
+        return maxPathSum_rec.maxSum
 
-            return root.val + max(leftTree, rightTree)
-        
-        max_sum_helper.max_sum = float("-inf")
 
-        max_sum_helper(root)
 
-        return max_sum_helper.max_sum
+

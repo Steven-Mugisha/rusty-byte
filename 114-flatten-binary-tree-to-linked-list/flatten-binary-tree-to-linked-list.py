d2cdfb91b18391a724cd1b0b9e427a15a38660cf
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-05-30 22:47:27 -0400

    Time: 40 ms (44.90%) | Memory: 16.8 MB (46.82%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
@@ -9,19 +9,21 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
-        if root is None: return None
+        if not root: return
+        
+        currentNode = root
 
-        curr_node = root
-
-        while curr_node:
-            if curr_node.left:
-                last = curr_node.left
+        while currentNode:
+            if currentNode.left:
+                last = currentNode.left
                 while last.right:
                     last = last.right
-                
-                last.right = curr_node.right
-                curr_node.right = curr_node.left
-                curr_node.left = None
-            curr_node = curr_node.right
+                last.right = currentNode.right
+                currentNode.right = currentNode.left
+                currentNode.left = None
+            
+            currentNode = currentNode.right
         
-        return root
+        return currentNode
+                
+

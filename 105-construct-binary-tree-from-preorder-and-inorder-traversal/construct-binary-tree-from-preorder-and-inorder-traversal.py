c3c05018a24d92aa05d2ee156f2bc8e9dd96cc4e
Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-06-11 22:30:54 -0400

    Time: 56 ms (80.07%) | Memory: 18 MB (90.15%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -8,24 +8,26 @@ class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         def buildTreeHelper(preorder, inorder, left, right, mapping, p_index):
             if left > right:
-                return 
-
-            curr = preorder[p_index[0]]
+                return None
+            
+            curr_node = preorder[p_index[0]]
             p_index[0] += 1
-            in_index = mapping[curr]
 
-            root = TreeNode(curr)
-            if left == right: return root
+            root = TreeNode(curr_node)
 
-            root.left = buildTreeHelper(preorder, inorder, left, in_index - 1, mapping, p_index)
-            root.right = buildTreeHelper(preorder, inorder, in_index + 1, right, mapping, p_index)
+            if left == right:
+                return root
+            
+            i_index = mapping[curr_node]
+            root.left = buildTreeHelper(preorder, inorder, left, i_index - 1, mapping, p_index)
+            root.right = buildTreeHelper(preorder, inorder, i_index + 1, right, mapping, p_index)
 
             return root
         
-        mapping, p_index = {} , [0]
+        mapping, p_index = {}, [0]
         for i in range(len(preorder)):
             mapping[inorder[i]] = i
         
         return buildTreeHelper(preorder, inorder, 0, len(preorder) - 1, mapping, p_index)
-
-
+            
+            

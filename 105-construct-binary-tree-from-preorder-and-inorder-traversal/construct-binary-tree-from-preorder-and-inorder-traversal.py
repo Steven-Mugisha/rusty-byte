58e381e0708d2601ed63cc3a6c0d63a2f1a185f4
Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-06-12 21:13:42 -0400

    Time: 74 ms (74.29%) | Memory: 17.7 MB (98.79%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,13 +6,29 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder or not inorder:
-            return None
-        
-        root = TreeNode(preorder[0])
-        mid = inorder.index(preorder[0])
-        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
-        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+        def build_helper(preorder, inorder, low, high, p_index):
+            if low > high:
+                return None
+            
+            current_value = preorder[p_index[0]]
+            p_index[0] += 1
 
-        return root
-    
+            root = TreeNode(current_value)
+
+            mid_index = inorder.index(current_value)
+
+            if low == high: return root
+
+            root.left = build_helper(preorder, inorder, low, mid_index - 1, p_index)
+            root.right = build_helper(preorder, inorder, mid_index + 1, high, p_index)
+
+            return root
+        
+        p_index = [0]
+
+        return build_helper(preorder, inorder, 0, len(preorder) - 1, p_index)
+
+            
+
+            
+            

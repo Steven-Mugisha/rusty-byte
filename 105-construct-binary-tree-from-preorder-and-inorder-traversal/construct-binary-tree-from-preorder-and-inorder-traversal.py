78adbd50a4f662bf6eaed0f99c7524a101590f1e
Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-06-16 22:11:08 -0400

    Time: 51 ms (87.58%) | Memory: 18.1 MB (89.63%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,29 +6,28 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        def build_helper(preorder, inorder, low, high, p_index):
+        def dfs(pre, ino, low, high, mapIdx, pIdx):
             if low > high:
-                return None
+                return
             
-            current_value = preorder[p_index[0]]
-            p_index[0] += 1
+            currVal = pre[pIdx[0]]
+            pIdx[0] += 1
 
-            root = TreeNode(current_value)
-
-            mid_index = inorder.index(current_value)
+            root = TreeNode(currVal)
 
             if low == high: return root
 
-            root.left = build_helper(preorder, inorder, low, mid_index - 1, p_index)
-            root.right = build_helper(preorder, inorder, mid_index + 1, high, p_index)
+            midIdx = mapIdx[currVal]
+
+            root.left = dfs(pre, ino, low, midIdx - 1, mapIdx, pIdx)
+            root.right = dfs(pre, ino, midIdx + 1, high, mapIdx, pIdx)
 
             return root
         
-        p_index = [0]
+        mapIdx = dict()
+        pIdx = [0]
 
-        return build_helper(preorder, inorder, 0, len(preorder) - 1, p_index)
-
-            
-
-            
-            
+        for val in range(len(preorder)):
+            mapIdx[inorder[val]] = val
+        
+        return dfs(preorder, inorder, 0, len(preorder) - 1, mapIdx, pIdx)

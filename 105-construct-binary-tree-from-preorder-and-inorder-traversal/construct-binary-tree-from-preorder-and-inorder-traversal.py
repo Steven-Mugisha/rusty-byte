1d825e64516aac58e52b28a6ff1a250697f4bb2f
Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-06-18 07:49:09 -0400

    Time: 141 ms (22.78%) | Memory: 88.2 MB (34.84%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,28 +6,12 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        def dfs(pre, ino, low, high, mapIdx, pIdx):
-            if low > high:
-                return
-            
-            currVal = pre[pIdx[0]]
-            pIdx[0] += 1
 
-            root = TreeNode(currVal)
+        if not preorder or not inorder: return
 
-            if low == high: return root
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
 
-            midIdx = mapIdx[currVal]
-
-            root.left = dfs(pre, ino, low, midIdx - 1, mapIdx, pIdx)
-            root.right = dfs(pre, ino, midIdx + 1, high, mapIdx, pIdx)
-
-            return root
-        
-        mapIdx = dict()
-        pIdx = [0]
-
-        for val in range(len(preorder)):
-            mapIdx[inorder[val]] = val
-        
-        return dfs(preorder, inorder, 0, len(preorder) - 1, mapIdx, pIdx)
+        return root        

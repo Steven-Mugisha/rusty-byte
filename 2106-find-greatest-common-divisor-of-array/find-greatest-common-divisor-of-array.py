8afd20e4a88a292e9e729c5ea37b15b24daa9098
Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-07-02 09:13:48 -0400

    Time: 59 ms (28.38%) | Memory: 16.8 MB (9.27%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.py
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.py
@@ -1,14 +1,17 @@
 class Solution:
     def findGCD(self, nums: List[int]) -> int:
+        # BaseCase: When equal return any number (max or min)
+        # RecursiveCase: When either max or min is higher reduce it until it reaches the BaseCase.
+
         def gcdHelper(minVal, maxVal):
             if minVal == maxVal:
                 return minVal
             
-            elif maxVal > minVal:
-                return gcdHelper(minVal, maxVal -  minVal)
-            
             else:
-                return gcdHelper(minVal -  maxVal, maxVal)
+                if minVal < maxVal:
+                    return gcdHelper(minVal, maxVal - minVal)
+                
+                else:
+                    return gcdHelper(minVal - maxVal, minVal)
         
         return gcdHelper(min(nums), max(nums))
-        

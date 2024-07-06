7249f40bc350274b68df8f2ea9b5013fe0cea1f8
Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-07-05 20:23:14 -0400

    Time: 38 ms (36.94%) | Memory: 16.5 MB (18.70%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -1,19 +1,14 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
 
-        stack = [[1]]
         if rowIndex == 0:
-            return stack[-1]
+            return [1]
         
-        for i in range(1, rowIndex + 1):
-            temp_arr = [1]
-            lastLine = stack[-1]
-            for j in range(len(lastLine) - 1):
-                temp_arr.append(lastLine[j] + lastLine[j+1])
-            temp_arr += [1]
-            stack.append(temp_arr)
-        
-        return stack[-1]
-        
-
-    
+        else:
+            line = [1]
+            previousLine = self.getRow(rowIndex -1)
+            for i in range(len(previousLine) - 1):
+                line.append(previousLine[i] + previousLine[i+1])
+            line += [1]
+            
+        return line

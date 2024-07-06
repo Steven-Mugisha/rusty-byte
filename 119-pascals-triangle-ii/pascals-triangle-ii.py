01019b06bb45cec47754b72bd31ded0c89fb8ab7
Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-07-05 23:09:18 -0400

    Time: 27 ms (94.46%) | Memory: 16.5 MB (18.70%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -1,14 +1,13 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-
         if rowIndex == 0:
             return [1]
         
         else:
             line = [1]
-            previousLine = self.getRow(rowIndex -1)
-            for i in range(len(previousLine) - 1):
-                line.append(previousLine[i] + previousLine[i+1])
+            prevRow = self.getRow(rowIndex -1)
+            for i in range(len(prevRow) - 1):
+                line.append(prevRow[i] + prevRow[i+1])
             line += [1]
-            
+        
         return line

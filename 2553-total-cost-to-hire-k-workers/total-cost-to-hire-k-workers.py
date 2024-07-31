f55d6e78400bc36804909031b7e70fb3de3ea6a6
Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-07-31 07:49:10 -0400

    Time: 661 ms (35.67%) | Memory: 28.4 MB (31.77%) - LeetSync

--- 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.py
+++ 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.py
@@ -1,29 +1,28 @@
 class Solution:
     def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
-        
-        heap = []
+
+        minHeap = []
 
         for i in range(candidates):
-            heapq.heappush(heap, (costs[i], 0))
+            heappush(minHeap, (costs[i], 0))
         
-        for i in range(max(candidates, len(costs) - candidates), len(costs)):
-            heapq.heappush(heap, (costs[i], 1))
+        for j in range(max(candidates, len(costs) - candidates), len(costs)):
+            heappush(minHeap, (costs[j], 1))
         
-        res = 0
+        ans = 0
         left = candidates
         right = len(costs) - candidates - 1
 
-
         for _ in range(k):
-            min_cost, direction = heapq.heappop(heap)
-            res += min_cost
+            min_cost, direction = heappop(minHeap)
+            ans += min_cost
+
             if left <= right:
                 if direction == 0:
-                    heapq.heappush(heap, (costs[left], 0))
+                    heappush(minHeap, (costs[left], 0))
                     left += 1
-                
                 else:
-                    heapq.heappush(heap, (costs[right], 1))
+                    heappush(minHeap, (costs[right], 1))
                     right -= 1
         
-        return res
+        return ans

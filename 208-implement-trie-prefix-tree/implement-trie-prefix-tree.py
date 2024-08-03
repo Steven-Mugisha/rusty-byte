eeec265a0b16cbff8cdf4af86756e55d36f04517
Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-08-03 13:54:46 -0400

    Time: 142 ms (27.45%) | Memory: 34 MB (6.14%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -1,42 +1,39 @@
 class TrieNode:
-    def __init__(self, ch = ''):
+    def __init__(self):
         self.children = [None]*26
         self.end = False
 
 class Trie:
     def __init__(self):
         self.root = TrieNode()
-    
-    def __get_index(self, char):
-        return ord(char) - ord('a')
         
     def insert(self, word: str) -> None:
-        current = self.root
-        for char in word:
-            index = self.__get_index(char)
-            if current.children[index] == None:
-                current.children[index] = TrieNode()
-            current = current.children[index]
-        current.end = True
-        
+        curr = self.root
+        for c in word:
+            index = ord(c) - ord('a')
+            if curr.children[index] == None:
+                curr.children[index] = TrieNode()
+            curr = curr.children[index]
+        curr.end = True
+
     def search(self, word: str) -> bool:
-        current = self.root
-        for char in word:
-            index = self.__get_index(char)
-            if current.children[index] == None:
+        curr = self.root
+        for c in word:
+            index = ord(c) - ord('a')
+            if curr.children[index] == None:
                 return False
-            current = current.children[index]
-        return current.end
-
+            curr = curr.children[index]
+        return curr.end
+        
     def startsWith(self, prefix: str) -> bool:
-        current = self.root
-        for char in prefix:
-            index = self.__get_index(char)
-            if current.children[index] == None:
+        curr = self.root
+        for c in prefix:
+            index = ord(c) - ord('a')
+            if curr.children[index] == None:
                 return False
-            current = current.children[index]
+            curr = curr.children[index]
         return True
-
+        
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
 # obj.insert(word)

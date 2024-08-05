931aeefc4efb122a8026f97858f4be800218d846
Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-08-05 08:28:52 -0400

    Time: 69 ms (6.49%) | Memory: 16.6 MB (63.80%) - LeetSync

--- 1547-destination-city/destination-city.py
+++ 1547-destination-city/destination-city.py
@@ -1,23 +1,5 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        
-        # create graph {london: 1, NewYork: 1, Lima:1, SaoPaulo:0}
-
-        # out, and in
-
-        # out_paths = defaultdict()
-        # in_paths = defaultdict()
-
-        # for start, end in enumerate(paths):
-        #     if out_paths.get(start) == 0:
-        #         out_paths[start] = 1
-            
-        #     else:
-        #         out_paths[start] += 1
-        
-        # print(f'dict {out_paths}')
-
-
         out = set()
 
         for start, end in paths:

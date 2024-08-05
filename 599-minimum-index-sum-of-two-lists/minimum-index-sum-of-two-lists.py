e97bfae4fb7189ff837bf8e8be74a39b8d1b208e
Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-08-05 08:01:36 -0400

    Time: 144 ms (71.58%) | Memory: 16.9 MB (98.97%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
@@ -1,25 +1,21 @@
 class Solution:
-    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-        
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:      
         map = {}
         minVal = float('inf')
         res = []
-
 
         for idx, string in enumerate(list1):
             map[string] = idx
         
         for idx, string in enumerate(list2):
-            if string in map.keys():
-                if (idx + map[string]) <= minVal:
-                    minVal = idx + map[string]
-        
-        for idx, string in enumerate(list2):
-            if string in map.keys():
-                if (idx + map[string]) <= minVal:
+            if string in map:
+                totalIdx = idx + map[string]
+
+                if totalIdx < minVal:
+                    minVal = totalIdx
+                    res = [string]
+                
+                elif totalIdx == minVal:
                     res.append(string)
         
         return res
-
-
-            

2cb37704967dec8e2a287955465ef9d26073891a
Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-08-06 07:59:52 -0400

    Time: 274 ms (79.80%) | Memory: 24.9 MB (89.90%) - LeetSync

--- 1455-filter-restaurants-by-vegan-friendly-price-and-distance/filter-restaurants-by-vegan-friendly-price-and-distance.py
+++ 1455-filter-restaurants-by-vegan-friendly-price-and-distance/filter-restaurants-by-vegan-friendly-price-and-distance.py
@@ -1,17 +1,17 @@
 class Solution:
     def filterRestaurants(self, restaurants: List[List[int]], veganFriendly: int, maxPrice: int, maxDistance: int) -> List[int]:
-        ratings, result = [], []
+        ratings = []
 
-        for r in restaurants:
-            if r[3] <= maxPrice and r[4] <= maxDistance:
-                if veganFriendly == 0 or veganFriendly == r[2]:
-                    ratings.append((r[1], r[0]))
+        for resto in restaurants:
+            if resto[3] <= maxPrice and resto[4] <= maxDistance:
+                if veganFriendly == 0 or resto[2] == veganFriendly:
+                    ratings.append((resto[1], resto[0]))
         
         ratings.sort()
 
-        for rate, id in ratings:
-            result.append(id)
-        
-        return result[::-1]
-        
+        ans = []
 
+        for rate, id in ratings:
+            ans.append(id)
+        
+        return ans[::-1]

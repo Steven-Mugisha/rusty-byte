c4b5add9b4eb77f24d1451c66b552305214f3b25
Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-08-14 09:35:40 -0400

    Time: 37 ms (46.54%) | Memory: 16.5 MB (31.84%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.py
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -1,25 +1,46 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        stack1, stack2 = [], []
+        p1, p2 = len(s)-1, len(t)-1
 
-        for c in s:
-            if len(stack1) != 0 and c == '#':
-                stack1.pop()
-            elif c != '#':
-                stack1.append(c)
-        
-        for c in t:
-            if len(stack2) != 0 and c == '#':
-                stack2.pop()
+        while p1 >= 0 or p2 >= 0:
+            backspace_count = 0
+
+            while p1 >= 0:
+                if s[p1] == '#':
+                    backspace_count += 1
+                    p1 -= 1
+                
+                elif backspace_count > 0:
+                    backspace_count -= 1
+                    p1 -= 1
+                
+                else:
+                    break
             
-            elif c != '#':
-                stack2.append(c)
-        
-        if stack1 == stack2:
-            return 1
-        
-        else:
-            return 0
+            backspace_count = 0
+            while p2 >= 0:
+                if t[p2] == '#':
+                    backspace_count += 1
+                    p2 -= 1
+                
+                elif backspace_count > 0:
+                    backspace_count -= 1
+                    p2 -= 1
+                
+                else:
+                    break
             
+            if p1 >= 0 and p2 >= 0 and s[p1] != t[p2]:
+                return False
+            
+            if (p1 >=0) != (p2 >=0):
+                return False
+            
+            p1 -= 1
+            p2 -= 1
+            
+        
+        return True
+        
      
             

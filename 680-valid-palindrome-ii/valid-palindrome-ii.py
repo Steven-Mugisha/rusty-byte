c8327c6f34d75cba248ffe919b10a6d97daa43a6
Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-08-26 22:31:19 -0400

    Time: 98 ms (64.72%) | Memory: 17 MB (67.55%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -8,20 +8,11 @@ class Solution:
                 i += 1
                 j -= 1
             return True
-        
-        i = 0 
+        i = 0
         j = len(s) - 1
-
         while i < j:
             if s[i] != s[j]:
                 return check_palindrome(s, i, j - 1) or check_palindrome(s, i + 1, j)
-            
             i += 1
             j -= 1
-        
         return True
-
-
-
-            
-

50e81a9952622a3c8fdecf27d6e39fbc7ced9b40
Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-09-16 19:49:48 -0400

    Time: 155 ms (52.53%) | Memory: 18.9 MB (11.89%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,11 +1,11 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         l, r = 0, len(nums) - 1
-        # temp = len(nums) - 1
+        temp = len(nums) - 1
 
         res = [0]*len(nums)
 
-        for i in reversed(range(len(nums))):
+        for _ in reversed(range(len(nums))):
             if abs(nums[r] >= abs(nums[l])):
                 val = nums[r]
                 r -= 1
@@ -14,8 +14,8 @@ class Solution:
                 val = nums[l]
                 l += 1
             
-            res[i] = val**2
-        
+            res[temp] = val**2
+            temp -= 1
         return res
 
 

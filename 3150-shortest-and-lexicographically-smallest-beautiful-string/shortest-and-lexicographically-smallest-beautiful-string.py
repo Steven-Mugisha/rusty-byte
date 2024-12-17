085713cfdc8789a98fc8f91b992932793b84c6c8
Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2024-12-17 15:35:12 -0400

    Time: 1 ms (67.37%) | Memory: 17.9 MB (11.46%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.py
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.py
@@ -1,31 +1,31 @@
 class Solution:
     def shortestBeautifulSubstring(self, s: str, k: int) -> str:
         l, r = 0, 0
-        ones_count = 0
-        shortest_length = float('inf')
+
+        count_ones = 0
+        shortest_length = float("inf")
         ans = []
 
         while r < len(s):
             if s[r] == '1':
-                ones_count += 1
-
-            while ones_count == k:  # Valid substring
+                count_ones += 1
+            
+            while count_ones == k:
                 current_length = r - l + 1
                 if current_length <= shortest_length:
                     if current_length < shortest_length:
-                        ans.clear()  # Clear previous results if a shorter length is found
+                        ans.clear()
                     shortest_length = current_length
                     ans.append(s[l:r + 1])
 
                 if s[l] == '1':
-                    ones_count -= 1
-
+                    count_ones -= 1
+                
                 l += 1
-
+                
             r += 1
-
+        
         if not ans:
             return ""
-
-        # Return lexicographically smallest substring
+        
         return min(ans)

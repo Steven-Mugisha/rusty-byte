2d6cb055ea4ac69dd3de8b27e0222668bc4e2b1d
Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2025-03-18 16:25:43 -0400

    Time: 151 ms (45.96%) | Memory: 78.3 MB (76.67%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,47 +1,24 @@
-class ListNode:
+class LinkedNode:
     def __init__(self, key, val):
         self.key = key
         self.val = val
         self.next = None
         self.prev = None
 
-
 class LRUCache:
+
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.dic = {}
-        self.head = ListNode(-1, -1)
-        self.tail = ListNode(-1, -1)
+        self.lookup = {}
+        self.head = LinkedNode(-1, -1)
+        self.tail = LinkedNode(-1, -1)
         self.head.next = self.tail
         self.tail.prev = self.head
-
-    def get(self, key: int) -> int:
-        if key not in self.dic:
-            return -1
-
-        node = self.dic[key]
-        self.remove(node)
-        self.add(node)
-        return node.val
-
-    def put(self, key: int, value: int) -> None:
-        if key in self.dic:
-            old_node = self.dic[key]
-            self.remove(old_node)
-
-        node = ListNode(key, value)
-        self.dic[key] = node
-        self.add(node)
-
-        if len(self.dic) > self.capacity:
-            node_to_delete = self.head.next
-            self.remove(node_to_delete)
-            del self.dic[node_to_delete.key]
-
+    
     def add(self, node):
-        previous_end = self.tail.prev
-        previous_end.next = node
-        node.prev = previous_end
+        prev_node = self.tail.prev
+        prev_node.next = node
+        node.prev = prev_node
         node.next = self.tail
         self.tail.prev = node
 
@@ -49,7 +26,33 @@ class LRUCache:
         node.prev.next = node.next
         node.next.prev = node.prev
 
+    def get(self, key: int) -> int:
+        if key not in self.lookup:
+            return -1
+        
+        current_used_node = self.lookup[key]
+        self.remove(current_used_node)
+        self.add(current_used_node)
 
+        return current_used_node.val
+
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.lookup:
+            old_node = self.lookup[key]
+            self.remove(old_node)
+        
+        node = LinkedNode(key, value)
+        self.lookup[key] = node
+        self.add(node)
+
+
+        if len(self.lookup) > self.capacity:
+            node_to_delete = self.head.next
+            self.remove(node_to_delete)
+            del self.lookup[node_to_delete.key]
+    
+        
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)

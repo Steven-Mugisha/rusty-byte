465956a2bb7afc256540de741c4bb86289f22c9c
Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2025-03-17 20:47:21 -0400

    Time: 325 ms (9.72%) | Memory: 25.1 MB (33.60%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,7 +1,6 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-
         island, visited = 0, set()
 
         def bfs(r,c):
@@ -10,18 +9,17 @@ class Solution:
 
             while q:
                 row, col = q.popleft()
-                directions = [(1,0), (-1, 0), (0, 1), (0,-1)]
+                directions = [(1,0), (-1, 0), (0, 1), (0, -1)]
 
-                for dr, dc in directions :
-                    R,C = row + dr,  col + dc
-
-                    if (R in range(ROWS) and C in range(COLS) and (R,C) not in visited and grid[R][C] == "1"):
-                        visited.add((R, C))
+                for dr, dc in directions:
+                    R, C = dr + row, dc + col
+                    if ((R,C) not in visited and R in range(ROWS) and C in range(COLS) and grid[R][C] == "1"):
+                        visited.add((R,C))
                         q.append((R,C))
 
         for r in range(ROWS):
             for c in range(COLS):
-                if (r, c) not in visited and grid[r][c] == "1":
+                if (r,c) not in visited and grid[r][c] == "1":
                     bfs(r,c)
                     island += 1
         return island

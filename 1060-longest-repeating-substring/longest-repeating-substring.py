65ad76a9d0918a50e3e716a6904de982655e6e44
Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2025-04-07 08:52:56 -0400

    Time: 3 ms (97.33%) | Memory: 18.4 MB (71.12%) - LeetSync

---
+++ 1060-longest-repeating-substring/longest-repeating-substring.py
@@ -0,0 +1,29 @@
+class Solution:
+    def longestRepeatingSubstring(self, s: str) -> int:
+
+        def exists(s, mid):
+            chuncks_set = set()
+    
+            for i in range(len(s) - mid + 1):
+                substring = s[i:i+mid]
+
+                if substring in chuncks_set:
+                    return True
+                
+                chuncks_set.add(substring)
+
+            
+            return False
+
+        start, end = 1, len(s) - 1
+
+        while start <= end:
+            mid = (start + end) //2
+
+            if exists(s, mid):
+                start = mid + 1
+            
+            else:
+                end = mid - 1
+        
+        return start - 1

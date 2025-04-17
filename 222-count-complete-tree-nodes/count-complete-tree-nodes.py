6b8e87ae01a97df63cebd199008deab22aeb6a30
Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2025-04-17 08:34:10 -0400

    Time: 0 ms (100.00%) | Memory: 23.4 MB (38.65%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.py
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.py
@@ -6,53 +6,22 @@
 #         self.right = right
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
+        def getHeight(node):
+            height = 0
+            while node:
+                height += 1
+                node = node.left
+            return height
+
         if not root:
             return 0
-        
-        d = self.compute_depth(root)
 
-        if d == 0:
-            return 1
-        
-        left, right = 1, 2**d - 1
-        while left <= right:
-            pivot = left + (right - left) // 2
-            if self.exists(pivot, d, root):
-                left = pivot + 1
-            
-            else:
-                right = pivot - 1
-        
+        left_height = getHeight(root.left)
+        right_height = getHeight(root.right)
 
-        return (2**d -1) + left
-
-    
-    def compute_depth(self, node):
-        d = 0
-        while node.left:
-            node = node.left
-            d += 1
-        return d
-    
-    def exists(self, idx, d, node):
-        left, right = 0, 2**d - 1
-
-        for _ in range(d):
-            pivot = left + (right - left) // 2
-            if idx <= pivot:
-                node = node.left
-                right = pivot
-            
-            else:
-                node = node.right
-                left = pivot + 1
-        return node is not None
-
-    
-    
-
-
-
-
-
-
+        if left_height == right_height:
+            # Left subtree is perfect
+            return (1 << left_height) + self.countNodes(root.right)
+        else:
+            # Right subtree is perfect
+            return (1 << right_height) + self.countNodes(root.left)
